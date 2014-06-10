8651728439cf870014e80c63ff9a6b3a024a93f6
ryoash <ashizawaryousuke@hotmail.com>
2014-06-11 01:37:14 +0900

    delete group folder

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,6 @@ fs.readFile('./views/_templates/header.html', 'UTF-8', function (err, data) {
 /* GET users listing. */
 var renderList = function(req, res) {
     var db = req.db, groups = db.groups;
-
     groups.find({}, function(err, items){
        res.render('admin/index', {title: 'Lunch Timer!', header: _templates.header, groups: items});
     });
@@ -25,35 +24,35 @@ router.get('/login', function(req, res){
     res.render('admin/login', { title: 'Login page for Lunch Timer administration' });
 });
 
-router.get('/group/add', function(req, res) {
-    // TODO: check token
 
-    // TODO: return json
 
+router.get('/ui/add', function(req, res) {
+    res.render('admin/add', {title: 'Add a Group'});
 });
 
-router.get('/group/edit/:id', function(req, res) {
+router.get('/ui/edit/:id', function(req, res) {
     // TODO: check token
-    res.render('group/add', {title: 'Edit a Group', id: req.param.id});
+    res.render('admin/add', {title: 'Edit a Group', id: req.param.id});
     // TODO: return json
-
 });
 
-router.post('/group/add', function(req, res) {
+router.post('/add', function(req, res) {
     // TODO: check token
     var entry = req.body;
     if(entry.name){
         req.db.groups.insert({name: entry.name,members: entry.members,restaurants: entry.restaurants}, function(err, newDoc){});
     }
+        renderList(req, res);
     // TODO: return json
 });
 
-router.get('/group/delete/:id', function(req, res) {
+router.get('/delete/:id', function(req, res) {
     // TODO: check token
     var id = req.param("id");
     if(id){
         req.db.groups.remove({_id: id}, {}, function(err, numRemoved){});
     }
+    renderList(req, res);
     // TODO: return json
 });
 
