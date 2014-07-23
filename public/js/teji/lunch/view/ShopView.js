527c3bf67adeb925e42984e03a2e2ba06ba8b569
tejitak <tejitak@hotmail.com>
2014-07-23 11:48:06 +0900

    #39 show evernote button on each shop for only logged in user

--- public/js/teji/lunch/view/ShopView.js
+++ public/js/teji/lunch/view/ShopView.js
@@ -23,7 +23,7 @@ define(["backbone", "underscore", "jquery", "text!./templates/ShopView.html"], f
             if(!json.imageURL){
                 json.imageURL = this.defaultImgURL;
             }
-            this.$el.html(this.template({item: json, labels: lunch.constants.labels}));
+            this.$el.html(this.template({item: json, labels: lunch.constants.labels, contextPath: lunch.constants.config.CONTEXT_PATH, isEvernoteLogin: lunch.evernoteUser ? true : false}));
 
             if(!enableVote){
                 this.$(".fnBtnVote").addClass('disabled'); 
